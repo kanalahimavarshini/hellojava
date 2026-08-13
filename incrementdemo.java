2d39d5d9cef88174966a46a30ab28142ec934a64
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-13 21:58:06 +0530

    code4

---
+++ incrementdemo.java
@@ -0,0 +1,31 @@
+class incrementdemo {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        System.out.println(x);
+
+        x++;
+        System.out.println(x);
+
+        x++;
+        System.out.println(x);
+
+        x--;
+        System.out.println(x);
+    }
+}
+class incrementdemo {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        System.out.println(x++);
+        System.out.println(x);
+
+        x = 10;
+
+        System.out.println(++x);
+        System.out.println(x);
+    }
+}
