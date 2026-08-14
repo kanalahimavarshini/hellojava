03ba6c79a89093ad0a4334f4b87e3091dd87f6cf
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-14 17:46:32 +0530

    class logic

--- logicaldemo.java
+++ logicaldemo.java
@@ -143,3 +143,18 @@ class NestedIfDemo {
         }
     }
 }
+class Login {
+    public static void main(String[] args) {
+
+        String username = "admin";
+        String password = "java123";
+
+        if (username.equals("admin")) {
+
+            if (password.equals("java123")) {
+                System.out.println("Login Successful");
+            }
+
+        }
+    }
+}
