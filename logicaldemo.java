b1be8da721b249c54562ba64f836042a46b6aca2
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-14 17:42:38 +0530

    if else ex=2

--- logicaldemo.java
+++ logicaldemo.java
@@ -58,3 +58,15 @@ class Vote {
         }
     }
 }
+class Vote {
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
