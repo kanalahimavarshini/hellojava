93f826d80eeaa2712791032a8c1f3f35525f6145
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-14 17:39:05 +0530

    logical code

---
+++ logicaldemo.java
@@ -0,0 +1,12 @@
+class LogicalDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        System.out.println(age > 18 && age < 25);
+        System.out.println(age > 18 || age < 10);
+
+        boolean isStudent = true;
+        System.out.println(!isStudent);
+    }
+}
