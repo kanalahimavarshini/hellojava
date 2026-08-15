57187516bf5645faeacf953c593d7d3ca3b86ea7
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-15 13:15:32 +0530

    Example with Strings

--- logicaldemo.java
+++ logicaldemo.java
@@ -349,3 +349,13 @@ class ForEachDemo {
         }
     }
 }
+class Fruits {
+    public static void main(String[] args) {
+
+        String[] fruits = {"Apple", "Banana", "Mango"};
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
