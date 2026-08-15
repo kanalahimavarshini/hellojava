e75bc5b099e77947ba0f5bc433dfe92017fff100
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-15 13:11:59 +0530

    Find the Largest Element

--- logicaldemo.java
+++ logicaldemo.java
@@ -270,3 +270,21 @@ class ArraySum {
         System.out.println("Sum = " + sum);
     }
 }
+class LargestElement {
+    public static void main(String[] args) {
+
+        int[] numbers = {25, 10, 80, 45, 60};
+
+        int largest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            }
+
+        }
+
+        System.out.println("Largest = " + largest);
+    }
+}
