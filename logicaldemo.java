747b455ca856851af67a24523bb97229baa38e83
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-15 13:14:58 +0530

    foreach demo

--- logicaldemo.java
+++ logicaldemo.java
@@ -339,3 +339,13 @@ class ArrayPractice {
         System.out.println("Smallest = " + smallest);
     }
 }
+class ForEachDemo {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
