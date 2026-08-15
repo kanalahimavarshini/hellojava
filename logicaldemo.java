14c9ffd1f12e6d07e58a5ab90d44d0eda465fff6
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-15 13:12:25 +0530

    Find the Smallest Element

--- logicaldemo.java
+++ logicaldemo.java
@@ -288,3 +288,21 @@ class LargestElement {
         System.out.println("Largest = " + largest);
     }
 }
+class SmallestElement {
+    public static void main(String[] args) {
+
+        int[] numbers = {25, 10, 80, 45, 60};
+
+        int smallest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] < smallest) {
+                smallest = numbers[i];
+            }
+
+        }
+
+        System.out.println("Smallest = " + smallest);
+    }
+}
