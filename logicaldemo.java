0f7091210f16d14ad31f1116ec2cb109565cb99b
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-15 13:10:28 +0530

    Take 5 integers as input and print them

--- logicaldemo.java
+++ logicaldemo.java
@@ -230,3 +230,29 @@ class Numbers {
         }
     }
 }
+import java.util.Scanner;
+
+class MiniExercise {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        // Take input
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.println("You entered:");
+
+        // Print the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        sc.close();
+    }
+}
