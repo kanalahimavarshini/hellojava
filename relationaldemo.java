25ee7ccd7abf476d335055a7bf2cb1856cc514f1
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-16 14:35:12 +0530

    example

--- relationaldemo.java
+++ relationaldemo.java
@@ -187,3 +187,20 @@ class Demo {
 
     }
 }
+class Calculator {
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(multiply(4, 5));
+        System.out.println(multiply(2, 3, 4));
+
+    }
+}
