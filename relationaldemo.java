96f45d46f4bbe61a06361073dd224752f3ce84cd
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-16 14:30:54 +0530

    example

--- relationaldemo.java
+++ relationaldemo.java
@@ -83,3 +83,17 @@ class Square {
 
     }
 }
+class Demo {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(10, 20);
+
+        System.out.println(result);
+
+    }
+}
