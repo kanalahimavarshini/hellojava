b68501eaa87942b43cec35f25b55f607c291a95d
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-16 14:35:42 +0530

    Write overloaded methods named area.

--- relationaldemo.java
+++ relationaldemo.java
@@ -204,3 +204,20 @@ class Calculator {
 
     }
 }
+class Area {
+
+    static int area(int side) {
+        return side * side;
+    }
+
+    static int area(int length, int width) {
+        return length * width;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(area(5));
+        System.out.println(area(4, 6));
+
+    }
+}
