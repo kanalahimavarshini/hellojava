d80dc854798bc743699c88298760ac148937f560
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-16 14:33:32 +0530

    Different Number of Parameters

--- relationaldemo.java
+++ relationaldemo.java
@@ -153,3 +153,20 @@ class Cube {
 
     }
 }
+class Demo {
+
+    static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    static void add(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+
+    public static void main(String[] args) {
+
+        add(10, 20);
+        add(10, 20, 30);
+
+    }
+}
