40318a76b4d0ff1da4088a5d2c3d75e12bccecaa
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-16 14:28:37 +0530

    Add Two Numbers

--- relationaldemo.java
+++ relationaldemo.java
@@ -52,3 +52,17 @@ class Demo {
 
     }
 }
+class Demo {
+
+    static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+
+        add(10, 20);
+        add(5, 7);
+        add(100, 50);
+
+    }
+}
