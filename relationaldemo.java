febc42509951bf186325e634c1220e1af45bc088
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-17 17:26:04 +0530

    Parameterized Constructor

--- relationaldemo.java
+++ relationaldemo.java
@@ -355,3 +355,35 @@ class Student {
     }
 
 }
+class Student {
+
+    String name;
+    int age;
+
+    Student(String n, int a) {
+
+        name = n;
+        age = a;
+
+    }
+
+    void display() {
+
+        System.out.println(name);
+        System.out.println(age);
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Divya", 21);
+
+        s1.display();
+
+    }
+
+}
