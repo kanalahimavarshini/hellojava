c27f76e285fd445d9d3357117b98ffa123717efb
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-17 17:23:26 +0530

    Creating Multiple Objects

--- relationaldemo.java
+++ relationaldemo.java
@@ -268,3 +268,28 @@ public class Main {
 
     }
 }
+class Student {
+
+    String name;
+    int age;
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        s1.name = "Divya";
+        s1.age = 21;
+
+        s2.name = "Rahul";
+        s2.age = 22;
+
+        System.out.println(s1.name + " " + s1.age);
+        System.out.println(s2.name + " " + s2.age);
+
+    }
+}
