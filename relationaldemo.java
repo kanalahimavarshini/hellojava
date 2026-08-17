52fa29f1adea28a757eb24d744b8cff4fbaec2d8
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-17 17:23:06 +0530

    Example

--- relationaldemo.java
+++ relationaldemo.java
@@ -247,3 +247,24 @@ s1.name = "Divya";
 s1.age = 21;
 System.out.println(s1.name);
 System.out.println(s1.age);
+class Student {
+
+    String name;
+    int age;
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Divya";
+        s1.age = 21;
+
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+
+    }
+}
