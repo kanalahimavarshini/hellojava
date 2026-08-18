5f71658db0e01a82d874fd78957b65f8f5cd9262
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-18 18:26:33 +0530

    Calling Parent Class Methods

--- basic.java
+++ basic.java
@@ -382,3 +382,35 @@ public class Main {
     }
 
 }
+class Animal {
+
+    void sound() {
+        System.out.println("Animal Sound");
+    }
+
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+
+        super.sound();
+
+        System.out.println("Dog Bark");
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.sound();
+
+    }
+
+}
