6a2586ce9890786213b6107882a858e23d243af1
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-18 18:15:49 +0530

    code

--- basic.java
+++ basic.java
@@ -100,3 +100,31 @@ public class Main {
     }
 
 }
+class Vehicle {
+
+    void start() {
+        System.out.println("Vehicle Started");
+    }
+
+}
+
+class Car extends Vehicle {
+
+    void drive() {
+        System.out.println("Car is Driving");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        c.start();
+        c.drive();
+
+    }
+
+}
