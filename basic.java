95a62ff9cd435ccd480b7c5f9042df3a7c6b68c1
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-18 18:26:56 +0530

    Calling Parent Class Constructor

--- basic.java
+++ basic.java
@@ -414,3 +414,32 @@ public class Main {
     }
 
 }
+class Animal {
+
+    Animal() {
+        System.out.println("Animal Constructor");
+    }
+
+}
+
+class Dog extends Animal {
+
+    Dog() {
+
+        super();
+
+        System.out.println("Dog Constructor");
+
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+    }
+
+}
