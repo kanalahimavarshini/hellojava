d3e4757fa246dd9fc5f31d0b7c56630e937c2692
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-18 18:23:21 +0530

    Runtime Polymorphism

--- basic.java
+++ basic.java
@@ -324,3 +324,31 @@ public class Main {
     }
 
 }
+class Animal {
+
+    void sound() {
+        System.out.println("Animal sound");
+    }
+
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal a = new Dog();
+
+        a.sound();
+
+    }
+
+}
