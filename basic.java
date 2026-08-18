c03779a4048fddc982416414ecb9f21e13caf794
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-18 18:22:03 +0530

    Method Overriding

--- basic.java
+++ basic.java
@@ -247,3 +247,30 @@ public class Main {
     }
 
 }
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+
+    }
+
+}
