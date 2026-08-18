b1124900677cc73a8b29c4ab5900be30789015ca
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-18 18:22:28 +0530

    Without Overriding

--- basic.java
+++ basic.java
@@ -274,3 +274,26 @@ public class Main {
     }
 
 }
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+
+}
+
+class Dog extends Animal {
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.sound();
+
+    }
+
+}
