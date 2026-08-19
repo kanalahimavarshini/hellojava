0d3e1377551ca08833af749e9eadfd4cbe4ee885
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-19 17:36:57 +0530

    Practice Program

--- logicaldemo.java
+++ logicaldemo.java
@@ -430,3 +430,34 @@ public class Main {
     }
 
 }
+abstract class Vehicle {
+
+    abstract void start();
+
+    void stop() {
+        System.out.println("Vehicle Stopped");
+    }
+
+}
+
+class Bike extends Vehicle {
+
+    @Override
+    void start() {
+        System.out.println("Bike Started");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bike b = new Bike();
+
+        b.start();
+        b.stop();
+
+    }
+
+}
