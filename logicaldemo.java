5521d1f0f8843675accaf5551c73906a288d74c1
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-19 17:51:10 +0530

    code1

--- logicaldemo.java
+++ logicaldemo.java
@@ -545,3 +545,29 @@ public class Main {
     }
 
 }
+abstract class Animal {
+
+    abstract void sound();
+
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Bark");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal a = new Dog();
+
+        a.sound();
+
+    }
+
+}
