90f4f0a3fcbe40f54abcddb4a81673949cfb15e3
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-19 17:52:06 +0530

    code3

--- incrementdemo.java
+++ incrementdemo.java
@@ -43,3 +43,29 @@ class Dog implements Animal {
     }
 
 }
+interface Animal {
+
+    void sound();
+
+}
+
+class Dog implements Animal {
+
+    @Override
+    public void sound() {
+        System.out.println("Dog");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal a = new Dog();
+
+        a.sound();
+
+    }
+
+}
