4e00b74cf1a58750ff897668124d0d83b675d49d
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-19 17:53:02 +0530

    code5

--- incrementdemo.java
+++ incrementdemo.java
@@ -82,3 +82,34 @@ class Dog implements Animal {
 
 Animal a = new Dog();
 a.sound();
+abstract class Shape {
+
+    abstract void draw();
+
+    void display() {
+        System.out.println("Shape");
+    }
+
+}
+
+class Circle extends Shape {
+
+    @Override
+    void draw() {
+        System.out.println("Circle");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Circle c = new Circle();
+
+        c.draw();
+        c.display();
+
+    }
+
+}
