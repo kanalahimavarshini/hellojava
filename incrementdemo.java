be6182bfe8834d8debb1a04646bc643d6f516366
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-20 17:53:06 +0530

    The throws Keyword

--- incrementdemo.java
+++ incrementdemo.java
@@ -359,3 +359,25 @@ public class Main {
     }
 
 }
+import java.io.IOException;
+
+public class Main {
+
+    static void readFile() throws IOException {
+
+        throw new IOException("File not found");
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            readFile();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
