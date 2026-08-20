9e7e606cc01a074eb5327c7931eb3c4926691256
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-20 17:52:39 +0530

    The throw Keyword

--- incrementdemo.java
+++ incrementdemo.java
@@ -344,3 +344,18 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 15;
+
+        if (age < 18) {
+            throw new ArithmeticException("Not Eligible");
+        }
+
+        System.out.println("Eligible");
+
+    }
+
+}
