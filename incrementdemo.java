6c6462ff3083f8a821958c1bf191208bf04eb905
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-20 17:54:51 +0530

    Custom Exception

--- incrementdemo.java
+++ incrementdemo.java
@@ -408,3 +408,35 @@ public class Main {
     }
 
 }
+class InvalidAgeException extends Exception {
+
+    InvalidAgeException(String message) {
+        super(message);
+    }
+
+}
+
+public class Main {
+
+    static void checkAge(int age) throws InvalidAgeException {
+
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above.");
+        }
+
+        System.out.println("Eligible to vote");
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            checkAge(15);
+        }
+        catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
