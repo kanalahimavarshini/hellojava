c9e10cafe4c8b13e047d510963b6ef49b24eb64a
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-20 17:46:42 +0530

    Exception Handling

--- incrementdemo.java
+++ incrementdemo.java
@@ -168,3 +168,26 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            int a = 10;
+            int b = 0;
+
+            System.out.println(a / b);
+
+        }
+        catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero.");
+
+        }
+
+        System.out.println("Program Continues...");
+
+    }
+
+}
