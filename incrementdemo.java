0b0d493b932771632c97cde6c13124334cf31af5
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-21 22:21:47 +0530

    example2

--- incrementdemo.java
+++ incrementdemo.java
@@ -546,3 +546,21 @@ public class Main {
     }
 
 }
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+
+        list.set(1, "Orange");
+
+        System.out.println(list);
+
+    }
+
+}
