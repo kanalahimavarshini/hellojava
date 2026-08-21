f947aa06fd53b5e64ad82e7e5ebd6b589ff84f7d
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-21 22:19:55 +0530

    example

--- incrementdemo.java
+++ incrementdemo.java
@@ -510,3 +510,20 @@ public class Main {
     }
 
 }
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        System.out.println(list.get(0));
+
+    }
+
+}
