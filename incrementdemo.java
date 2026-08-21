5c6a742c7bd9483b3cf55c7a75af8cb3d51f37b7
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-21 22:19:01 +0530

    Adding Elements

--- incrementdemo.java
+++ incrementdemo.java
@@ -472,3 +472,20 @@ public class Main {
     }
 
 }
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+
+        System.out.println(fruits);
+
+    }
+
+}
