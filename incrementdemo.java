ed068838855e17bb3bda346476fa8338a40e2648
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-21 22:21:08 +0530

    example1

--- incrementdemo.java
+++ incrementdemo.java
@@ -527,3 +527,22 @@ public class Main {
     }
 
 }
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        numbers.remove(1);
+
+        System.out.println(numbers);
+
+    }
+
+}
