c018b62a4ebac6b27d15e98c5d24a346dd0da7f5
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-21 22:23:04 +0530

    code1

--- incrementdemo.java
+++ incrementdemo.java
@@ -581,3 +581,18 @@ public class Main {
     }
 
 }
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+
+        System.out.println(list.contains("Java"));
+    }
+
+}
