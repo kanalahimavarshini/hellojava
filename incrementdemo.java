a5ef9b4105d779cc070d701daef01de8a6e2e0a4
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-22 22:20:08 +0530

    Creating a File

--- incrementdemo.java
+++ incrementdemo.java
@@ -703,3 +703,27 @@ public class Main {
     }
 
 }
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("student.txt");
+
+            if (file.createNewFile()) {
+                System.out.println("File created.");
+            } else {
+                System.out.println("File already exists.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error creating file.");
+        }
+
+    }
+
+}
