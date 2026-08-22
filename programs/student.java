1137719200c513d00e56e393975443a1977a1512
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-22 22:29:16 +0530

    FileReader

--- programs/student.java
+++ programs/student.java
@@ -72,3 +72,31 @@ public class Main {
     }
 
 }
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileReader reader = new FileReader("student.txt");
+
+            int ch;
+
+            while ((ch = reader.read()) != -1) {
+                System.out.print((char) ch);
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Error reading file.");
+
+        }
+
+    }
+
+}
