4ebc7ef743ed0106fb8782c2396fc09471609b33
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-22 22:30:04 +0530

    BufferedReader

--- programs/student.java
+++ programs/student.java
@@ -100,3 +100,34 @@ public class Main {
     }
 
 }
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            BufferedReader br = new BufferedReader(
+                new FileReader("student.txt")
+            );
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Error");
+
+        }
+
+    }
+
+}
