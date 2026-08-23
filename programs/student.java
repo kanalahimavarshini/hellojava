80563187bc2a561a238ccfb0188adbdb685e13eb
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-23 14:06:14 +0530

    code1

--- programs/student.java
+++ programs/student.java
@@ -311,3 +311,22 @@ public class Main {
     }
 
 }
+class MyThread extends Thread {
+
+    public void run() {
+        System.out.println("Thread is running...");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyThread t = new MyThread();
+
+        t.start();
+
+    }
+
+}
