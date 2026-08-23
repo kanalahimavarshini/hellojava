3601587a31ad649221a08eae1062eb7cc71ed985
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-23 14:15:37 +0530

    code5

--- programs/student.java
+++ programs/student.java
@@ -451,3 +451,22 @@ public class Main {
     }
 
 }
+class MyTask implements Runnable {
+
+    public void run() {
+        System.out.println("Runnable thread");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Thread t = new Thread(new MyTask());
+
+        t.start();
+
+    }
+
+}
