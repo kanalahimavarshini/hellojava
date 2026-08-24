afb8b596e8b83725bcef2db80e7c5d5de92f919f
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-24 19:09:15 +0530

    Practice Program

--- programs/student.java
+++ programs/student.java
@@ -498,3 +498,31 @@ public class Main {
     }
 
 }
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/testdb",
+                "root",
+                "password"
+            );
+
+            System.out.println("Database Connected");
+
+            con.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Connection Failed");
+
+        }
+
+    }
+
+}
