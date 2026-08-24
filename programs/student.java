7a4453e43969334208ba7a400c03c8da3853f7aa
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-24 19:11:20 +0530

    code1

--- programs/student.java
+++ programs/student.java
@@ -554,3 +554,31 @@ public class Main {
     }
 
 }
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/studentdb",
+                "root",
+                "password"
+            );
+
+            System.out.println("Connection Successful!");
+
+            con.close();
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+    }
+
+}
