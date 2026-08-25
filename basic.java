4fe7b75a3e6ae1a2c69ed5f9e80bf4c7a189a20f
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-25 22:00:04 +0530

    Example

--- basic.java
+++ basic.java
@@ -610,3 +610,16 @@ public class Main {
     }
 
 }
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Function<Integer, Integer> square = n -> n * n;
+
+        System.out.println(square.apply(5));
+
+    }
+
+}
