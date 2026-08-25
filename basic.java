f4886fa9841e1da6b26c2028b398e14fcb79d2b2
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-25 21:49:48 +0530

    Practice Program

--- basic.java
+++ basic.java
@@ -470,3 +470,18 @@ public class Main {
     }
 
 }
+interface Square {
+    int square(int n);
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Square s = n -> n * n;
+
+        System.out.println(s.square(5));
+
+    }
+
+}
