a4f7897aa9f3be1657ef87e2b081d62c940df750
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-25 21:49:19 +0530

    Lambda with Parameters

--- basic.java
+++ basic.java
@@ -455,3 +455,18 @@ Greeting g = new Greeting() {
     }
 
 };
+interface Add {
+    int add(int a, int b);
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Add obj = (a, b) -> a + b;
+
+        System.out.println(obj.add(10, 20));
+
+    }
+
+}
