f696cd838878a418defeba1039aa87ae05195183
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-25 21:56:38 +0530

    Function

--- basic.java
+++ basic.java
@@ -528,3 +528,16 @@ public class Main {
     }
 
 }
+import java.util.function.Function;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Function<Integer, Integer> square = n -> n * n;
+
+        System.out.println(square.apply(6));
+
+    }
+
+}
