e5191391b2f74c111417d2ad4c5e74f054de6e3f
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-26 09:25:56 +0530

    Square numbers

--- basic.java
+++ basic.java
@@ -745,3 +745,18 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4);
+
+        nums.stream()
+            .map(n -> n * n)
+            .forEach(System.out::println);
+
+    }
+
+}
