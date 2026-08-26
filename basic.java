dfdb8f105b0eb44bfe63ab829c3713963d3d4744
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-26 09:27:58 +0530

    count()

--- basic.java
+++ basic.java
@@ -794,3 +794,19 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names =
+                Arrays.asList("Alice", "Bob", "Charlie");
+
+        long total = names.stream().count();
+
+        System.out.println(total);
+
+    }
+
+}
