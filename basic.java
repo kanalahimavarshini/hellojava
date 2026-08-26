acd0fc3e1d62d96a7eca04680cb3fffb166f551f
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-26 09:26:55 +0530

    example

--- basic.java
+++ basic.java
@@ -760,3 +760,21 @@ public class Main {
     }
 
 }
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> evenNumbers = nums.stream()
+                                        .filter(n -> n % 2 == 0)
+                                        .collect(Collectors.toList());
+
+        System.out.println(evenNumbers);
+
+    }
+
+}
