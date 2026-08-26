8e01e1352b464caecb2fe00a496189f3bf43a7ff
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-26 09:28:47 +0530

    Chaining Multiple Operations

--- logicaldemo.java
+++ logicaldemo.java
@@ -588,3 +588,25 @@ public class Main {
     }
 
 }
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums =
+                Arrays.asList(5, 10, 15, 20, 25, 30);
+
+        List<Integer> result =
+                nums.stream()
+                    .filter(n -> n >= 15)
+                    .map(n -> n * 2)
+                    .limit(3)
+                    .collect(Collectors.toList());
+
+        System.out.println(result);
+
+    }
+
+}
