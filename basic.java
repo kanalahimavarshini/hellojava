7b9bd8e8a8b5f3f95c08565c15d0a469bf3c12d4
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-26 09:27:37 +0530

    reduce()

--- basic.java
+++ basic.java
@@ -778,3 +778,19 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
+
+        int sum = nums.stream()
+                      .reduce(0, (a, b) -> a + b);
+
+        System.out.println(sum);
+
+    }
+
+}
