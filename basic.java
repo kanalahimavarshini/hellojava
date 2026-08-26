8c3ee7c69045f8b51567c32cb2e9011347a0e2a3
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-26 09:23:21 +0530

    Practice Program

--- basic.java
+++ basic.java
@@ -681,3 +681,23 @@ List<String> result =
              .collect(Collectors.toList());
 
 System.out.println(result);
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums =
+                Arrays.asList(2, 3, 4, 5, 6);
+
+        List<Integer> squares =
+                nums.stream()
+                    .map(n -> n * n)
+                    .collect(Collectors.toList());
+
+        System.out.println(squares);
+
+    }
+
+}
