9934f94861fcf550ccb6aa0033b760f775b0b518
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-26 09:24:00 +0530

    Example

--- basic.java
+++ basic.java
@@ -701,3 +701,17 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        names.stream()
+             .forEach(System.out::println);
+
+    }
+
+}
