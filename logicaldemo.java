e719c9c592708505672a76f68da70239c7ee3594
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-27 22:34:49 +0530

    ZonedDateTime

--- logicaldemo.java
+++ logicaldemo.java
@@ -725,3 +725,18 @@ public class Main {
     }
 
 }
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ZonedDateTime india =
+                ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+
+        System.out.println(india);
+
+    }
+
+}
