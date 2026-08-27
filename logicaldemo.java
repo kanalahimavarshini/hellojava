67975538e7d95a2f80b5ad71573f38c663de1ce5
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-27 22:35:18 +0530

    DateTimeFormatter

--- logicaldemo.java
+++ logicaldemo.java
@@ -740,3 +740,20 @@ public class Main {
     }
 
 }
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        System.out.println(today.format(formatter));
+
+    }
+
+}
