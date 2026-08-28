3d86c5c2349c33268b273f0189268ac48e33afd4
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-28 15:57:15 +0530

    code2

--- logicaldemo.java
+++ logicaldemo.java
@@ -1006,3 +1006,15 @@ public class Main {
     }
 
 }
+class Box<T> {
+
+    private T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+}
