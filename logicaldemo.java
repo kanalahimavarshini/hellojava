223c49c30ea21286a30ef7a4cfc7be60c646a784
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-28 15:47:09 +0530

    Without Generics

--- logicaldemo.java
+++ logicaldemo.java
@@ -788,3 +788,21 @@ public class Main {
     }
 
 }
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList list = new ArrayList();
+
+        list.add("Java");
+        list.add(100);
+
+        String s = (String) list.get(0);
+
+        System.out.println(s);
+
+    }
+
+}
