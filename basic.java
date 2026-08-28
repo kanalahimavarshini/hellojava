01bbdcc1fa90a49f48ff6563357254f368d027dd
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-28 15:58:23 +0530

    example

--- basic.java
+++ basic.java
@@ -810,3 +810,19 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static <T> void print(T value) {
+        System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+
+        print("Java");
+        print(100);
+        print(3.14);
+        print(true);
+
+    }
+
+}
