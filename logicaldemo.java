07556194e54dea9c7a97e489719ab8df1084f920
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-28 15:54:32 +0530

    code1

--- logicaldemo.java
+++ logicaldemo.java
@@ -958,3 +958,25 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void addNumbers(List<? super Integer> list) {
+
+        list.add(10);
+        list.add(20);
+
+    }
+
+    public static void main(String[] args) {
+
+        List<Number> numbers = new ArrayList<>();
+
+        addNumbers(numbers);
+
+        System.out.println(numbers);
+
+    }
+
+}
