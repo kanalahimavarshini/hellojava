01426dfacb00d9d4d54e5722ff355057f498feaa
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-29 21:29:21 +0530

    code1

--- incrementdemo.java
+++ incrementdemo.java
@@ -952,3 +952,33 @@ public class Main {
     }
 
 }
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileReader reader = new FileReader("message.txt");
+
+            int ch;
+
+            while ((ch = reader.read()) != -1) {
+
+                System.out.print((char) ch);
+
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+}
