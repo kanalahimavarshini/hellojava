32f443e60f0f8fa0fb89a9a3817efcf9a63aed27
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-29 21:30:04 +0530

    code2

--- incrementdemo.java
+++ incrementdemo.java
@@ -982,3 +982,29 @@ public class Main {
     }
 
 }
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileInputStream fis = new FileInputStream("sample.txt");
+
+            int data;
+
+            while ((data = fis.read()) != -1) {
+                System.out.print((char) data);
+            }
+
+            fis.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
