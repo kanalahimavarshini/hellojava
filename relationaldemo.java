385c5aedb15504642c94bad602d402ce5b04e2fa
Himavarshini <kanalahimavarshini@gmail.com>
2026-08-31 21:50:02 +0530

    code

--- relationaldemo.java
+++ relationaldemo.java
@@ -878,3 +878,19 @@ public class Main {
     }
 
 }
+class Animal {
+
+    void sound() {
+        System.out.println("Animal Sound");
+    }
+
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Bark");
+    }
+
+}
