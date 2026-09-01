9f66280381eef2bf8a48666d726750ae6591d72e
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-01 14:14:06 +0530

    example2

--- programs.java
+++ programs.java
@@ -87,3 +87,26 @@ public class Main {
     }
 
 }
+import java.net.InetAddress;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            InetAddress address =
+                    InetAddress.getByName("google.com");
+
+            System.out.println("Host Name: " + address.getHostName());
+            System.out.println("IP Address: " + address.getHostAddress());
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+    }
+
+}
