9e58a6247e3703523c7eb2d90a2ebfeff2106eb8
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-01 14:16:04 +0530

    Socket

--- programs.java
+++ programs.java
@@ -132,3 +132,27 @@ public class Main {
     }
 
 }
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Socket socket =
+                    new Socket("localhost", 5000);
+
+            System.out.println("Connected!");
+
+            socket.close();
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+    }
+
+}
