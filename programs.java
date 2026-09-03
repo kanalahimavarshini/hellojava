689908b32cad7f0c595cb2888c75b94f20f4a013
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-03 16:24:36 +0530

    code

--- programs.java
+++ programs.java
@@ -428,3 +428,20 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Alice");
+
+        System.out.println(names);
+
+    }
+
+}
