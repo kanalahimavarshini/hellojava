b61e766c6a5711b309051fe2ecc206574a5092af
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-03 16:27:59 +0530

    code3

--- programs.java
+++ programs.java
@@ -462,3 +462,21 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("A");
+        list.add("C");
+
+        list.add(1, "B");
+
+        System.out.println(list);
+
+    }
+
+}
