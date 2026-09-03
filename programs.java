ef84b68d71cc2796c1bae945feda9e1e460fccdf
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-03 16:25:16 +0530

    code1

--- programs.java
+++ programs.java
@@ -445,3 +445,20 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> languages = new ArrayList<>();
+
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("C++");
+
+        System.out.println(languages);
+
+    }
+
+}
