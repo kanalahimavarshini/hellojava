0cab9b58eb1db24b4d9744778f5f37c2474de34e
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-05 18:18:13 +0530

    code

--- programs.java
+++ programs.java
@@ -873,3 +873,22 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            int result = 10 / 0;
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero.");
+
+        }
+
+        System.out.println("Program continues.");
+
+    }
+
+}
