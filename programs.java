0832b2a2144841d5555135e83d27acfc3b0c4b4d
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-05 18:17:12 +0530

    throw

--- programs.java
+++ programs.java
@@ -852,3 +852,24 @@ try {
     System.out.println("Invalid index");
 
 }
+public class Main {
+
+    static void checkAge(int age) {
+
+        if (age < 18) {
+
+            throw new ArithmeticException("Not eligible");
+
+        }
+
+        System.out.println("Eligible");
+
+    }
+
+    public static void main(String[] args) {
+
+        checkAge(15);
+
+    }
+
+}
