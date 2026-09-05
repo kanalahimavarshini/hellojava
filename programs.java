cebca9866275e32bb19fd6e1577c8a4ee29dec0a
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-05 18:19:55 +0530

    code1

--- programs.java
+++ programs.java
@@ -914,3 +914,21 @@ public class Main {
     }
 
 }
+public class Main {
+
+    static void checkAge(int age) {
+
+        if (age < 18) {
+            throw new ArithmeticException("Not eligible to vote");
+        }
+
+        System.out.println("Eligible to vote");
+    }
+
+    public static void main(String[] args) {
+
+        checkAge(15);
+
+    }
+
+}
