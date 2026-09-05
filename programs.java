8c0684783b685d7209e7c92b21448e29c09673e2
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-05 18:27:12 +0530

    code5

--- programs.java
+++ programs.java
@@ -979,3 +979,24 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            int result = 10 / 0;
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero.");
+
+        } finally {
+
+            System.out.println("Cleanup completed.");
+
+        }
+
+    }
+
+}
