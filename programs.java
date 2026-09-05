c67c4dff697bcadb2b01514f1d702d5d7d4cbd65
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-05 18:16:22 +0530

    try and catch

--- programs.java
+++ programs.java
@@ -817,3 +817,23 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            int result = 10 / 0;
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero.");
+
+        }
+
+        System.out.println("Program continues...");
+
+    }
+
+}
