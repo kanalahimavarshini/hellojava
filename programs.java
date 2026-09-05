66b793f406d6942a88b0598ad741450e204c97a7
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-05 18:23:56 +0530

    code2

--- programs.java
+++ programs.java
@@ -932,3 +932,17 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+
+        System.out.println(a / b); // Throws ArithmeticException
+
+        System.out.println("Program End");
+
+    }
+
+}
