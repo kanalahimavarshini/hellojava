58b836f18dae294113487ba24773e8777ece4733
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-06 17:57:30 +0530

    code1

--- prog.java
+++ prog.java
@@ -150,3 +150,20 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Apple");
+
+        System.out.println(fruits);
+
+    }
+
+}
