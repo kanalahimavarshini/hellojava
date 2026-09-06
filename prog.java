6827578d35a3425b9f435d4b92b63ef7be775fad
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-06 17:49:46 +0530

    ArrayList

---
+++ prog.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        System.out.println(fruits);
+
+    }
+
+}
