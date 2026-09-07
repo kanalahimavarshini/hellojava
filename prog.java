142cecc472cb502d74c435bb3a6ee5cd6c3c0ed0
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-07 21:14:43 +0530

    Example1

--- prog.java
+++ prog.java
@@ -240,3 +240,20 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> students = new HashMap<>();
+
+        students.put(101, "Alice");
+        students.put(102, "Bob");
+
+        System.out.println(students.get(101));
+        System.out.println(students.get(105));
+
+    }
+
+}
