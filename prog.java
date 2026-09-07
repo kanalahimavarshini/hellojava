fb6f2100d1436d45841afddd25427ea8da7f8c80
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-07 21:18:55 +0530

    Example4

--- prog.java
+++ prog.java
@@ -291,3 +291,20 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        map.put(3, "Three");
+        map.put(1, "One");
+        map.put(2, "Two");
+
+        System.out.println(map);
+
+    }
+
+}
