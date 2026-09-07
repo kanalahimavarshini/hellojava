a0687da9051e82d02c6d0f582cccace543853ccd
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-07 21:22:33 +0530

    code3

--- prog.java
+++ prog.java
@@ -376,3 +376,20 @@ public class Main {
     }
 
 }
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new LinkedHashMap<>();
+
+        map.put(3, "Three");
+        map.put(1, "One");
+        map.put(2, "Two");
+
+        System.out.println(map);
+
+    }
+
+}
