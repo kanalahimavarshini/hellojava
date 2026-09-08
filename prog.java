30c01e0c931ae4af14d2f9f53d3608d33f4b1d73
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-08 11:32:24 +0530

    code7

--- prog.java
+++ prog.java
@@ -482,3 +482,20 @@ public class Main {
     }
 
 }
+import java.io.*;
+
+public class Main {
+
+    static void readFile() throws IOException {
+
+        FileReader file = new FileReader("data.txt");
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Method declared with throws.");
+
+    }
+
+}
