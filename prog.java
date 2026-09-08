41a5f38f87661c59fc00b44adb3e997e057730e8
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-08 11:34:02 +0530

    code9

--- prog.java
+++ prog.java
@@ -516,3 +516,26 @@ public class Main {
     }
 
 }
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            System.out.println("Inside try");
+
+            int result = 10 / 0;
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero.");
+
+        } finally {
+
+            System.out.println("Finally block executed.");
+
+        }
+
+    }
+
+}
