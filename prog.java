47b278db147820cb06b5241f28272a59572e90b4
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-09 17:33:39 +0530

    Reading a File (FileReader)

--- prog.java
+++ prog.java
@@ -695,3 +695,32 @@ public class Main {
     }
 
 }
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileReader reader = new FileReader("data.txt");
+
+            int ch;
+
+            while ((ch = reader.read()) != -1) {
+
+                System.out.print((char) ch);
+
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Read error.");
+
+        }
+
+    }
+
+}
