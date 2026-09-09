766f281685d7e5d030c71116e06afd97a91b1c0d
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-09 17:37:07 +0530

    code1

--- prog.java
+++ prog.java
@@ -803,3 +803,18 @@ public class Main {
     }
 
 }
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("data.txt");
+
+        System.out.println(file.exists());
+        System.out.println(file.getName());
+        System.out.println(file.getAbsolutePath());
+
+    }
+
+}
