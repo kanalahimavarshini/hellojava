5b1b17e3c9deace2fa5ff6caa3dd1a23e3e7bcf7
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-09 17:42:57 +0530

    code8

--- prog.java
+++ prog.java
@@ -935,3 +935,26 @@ public class Main {
 
     }
 }
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("data.txt");
+
+            if (file.createNewFile()) {
+                System.out.println("File created successfully.");
+            } else {
+                System.out.println("File already exists.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+
+    }
+
+}
