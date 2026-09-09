0304a7c630cc9b7a67e5675b77849dd494f3c678
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-09 17:38:55 +0530

    code3

--- prog.java
+++ prog.java
@@ -842,3 +842,27 @@ public class Main {
     }
 
 }
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader reader =
+                 new BufferedReader(new FileReader("data.txt"))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("Read error.");
+
+        }
+
+    }
+
+}
