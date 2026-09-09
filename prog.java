1d002a00aaeadc677bcff2bcd65400e284220fe3
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-09 17:34:55 +0530

    Deleting a File

--- prog.java
+++ prog.java
@@ -782,3 +782,24 @@ public class Main {
     }
 
 }
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("data.txt");
+
+        if (file.delete()) {
+
+            System.out.println("Deleted successfully.");
+
+        } else {
+
+            System.out.println("Delete failed.");
+
+        }
+
+    }
+
+}
