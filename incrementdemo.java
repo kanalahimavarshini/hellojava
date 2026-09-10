12330c118476a417bd1af0c895202e0b9bb8e4b7
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-10 21:53:06 +0530

    code3

--- incrementdemo.java
+++ incrementdemo.java
@@ -1105,3 +1105,39 @@ class Student implements Serializable {
     int id;
     String name;
 }
+import java.io.*;
+
+class Student implements Serializable {
+
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s = new Student(101, "Alice");
+
+        try (ObjectOutputStream out =
+                 new ObjectOutputStream(
+                     new FileOutputStream("student.ser"))) {
+
+            out.writeObject(s);
+
+            System.out.println("Object serialized successfully.");
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
