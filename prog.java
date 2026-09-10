3f7f8d303feab1e3d2fd000ef12d1b14356c51d7
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-10 21:44:31 +0530

    Serializing an Object

--- prog.java
+++ prog.java
@@ -990,3 +990,40 @@ class Student implements Serializable {
     String name;
 
 }
+import java.io.*;
+
+class Student implements Serializable {
+
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s = new Student(101, "Alice");
+
+        try (ObjectOutputStream out =
+                 new ObjectOutputStream(
+                     new FileOutputStream("student.ser"))) {
+
+            out.writeObject(s);
+
+            System.out.println("Object saved.");
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
