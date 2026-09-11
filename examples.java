440801fc917d5551642d0b3f0fe1a82078223c4a
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-11 21:04:21 +0530

    9

--- examples.java
+++ examples.java
@@ -141,3 +141,19 @@ class Main {
         System.out.println(map);
     }
 }
+// 9. Maximum Subarray Sum
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int current = arr[0];
+        int maximum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            current = Math.max(arr[i], current + arr[i]);
+            maximum = Math.max(maximum, current);
+        }
+
+        System.out.println(maximum);
+    }
+}
