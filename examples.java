fc072f43712bb49f7fe61ed3ad4f85e04329234b
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-11 21:03:11 +0530

    5

--- examples.java
+++ examples.java
@@ -79,3 +79,15 @@ class Main {
         }
     }
 }
+// 5. Factorial using Recursion
+class Main {
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
