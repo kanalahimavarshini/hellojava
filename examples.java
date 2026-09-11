720bf5e0bde1b956773a8a10de09f210a5804730
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-11 21:02:55 +0530

    4

--- examples.java
+++ examples.java
@@ -66,3 +66,16 @@ class Main {
         System.out.println(prime);
     }
 }
+// 4. Fibonacci Series
+class Main {
+    public static void main(String[] args) {
+        int a = 0, b = 1;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
