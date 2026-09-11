a4d3430edc5f564297947bffb11bb157e3e7d594
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-11 21:02:39 +0530

    3

--- examples.java
+++ examples.java
@@ -50,3 +50,19 @@ class Main {
         System.out.println(temp == rev);
     }
 }
+// 3. Prime Number
+class Main {
+    public static void main(String[] args) {
+        int n = 29;
+        boolean prime = true;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        System.out.println(prime);
+    }
+}
