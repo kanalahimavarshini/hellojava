2c938100e88942b2e987ce179ac969da4082154d
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-11 21:03:34 +0530

    6

--- examples.java
+++ examples.java
@@ -91,3 +91,23 @@ class Main {
         System.out.println(factorial(5));
     }
 }
+// 6. Second Largest Element
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 20, 15};
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int n : arr) {
+            if (n > largest) {
+                second = largest;
+                largest = n;
+            } else if (n > second && n != largest) {
+                second = n;
+            }
+        }
+
+        System.out.println(second);
+    }
+}
