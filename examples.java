1a1b0303b5d37d4d1fbc11c13e9cf37666effa56
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-12 09:15:55 +0530

    21

--- examples.java
+++ examples.java
@@ -323,3 +323,22 @@ class Main {
         }
     }
 }
+// 17. Sort an Array using Bubble Sort
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
