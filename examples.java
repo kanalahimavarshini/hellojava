7c0b8cd4e318622e47a0b8926fbdf8430bc4f962
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-12 09:16:19 +0530

    22

--- examples.java
+++ examples.java
@@ -342,3 +342,19 @@ class Main {
             System.out.print(x + " ");
     }
 }
+// 18. Armstrong Number
+class Main {
+    public static void main(String[] args) {
+        int n = 153;
+        int temp = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        System.out.println(sum == temp);
+    }
+}
