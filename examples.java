a03beabceb170322727eb6d86d46b731523319e9
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-12 09:14:59 +0530

    19

--- examples.java
+++ examples.java
@@ -280,3 +280,24 @@ class Main {
         System.out.println(slow);
     }
 }
+// 15. Longest Substring Without Repeating Characters
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right)))
+                set.remove(s.charAt(left++));
+
+            set.add(s.charAt(right));
+            max = Math.max(max, right - left + 1);
+        }
+
+        System.out.println(max);
+    }
+}
