32f28eb791b7ce0ff3301f739e76d49670d07a20
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-12 09:14:22 +0530

    17

--- examples.java
+++ examples.java
@@ -238,3 +238,22 @@ class Main {
         System.out.println(total);
     }
 }
+// 13. Move Zeros to End
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+
+        int index = 0;
+
+        for (int x : arr) {
+            if (x != 0)
+                arr[index++] = x;
+        }
+
+        while (index < arr.length)
+            arr[index++] = 0;
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
