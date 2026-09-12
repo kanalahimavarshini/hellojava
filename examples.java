e129d91f372aafee2604ec86cb24b0f199e17317
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-12 09:15:36 +0530

    20

--- examples.java
+++ examples.java
@@ -301,3 +301,25 @@ class Main {
         System.out.println(max);
     }
 }
+// 16. Two Sum
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int need = target - arr[i];
+
+            if (map.containsKey(need)) {
+                System.out.println(map.get(need) + " " + i);
+                return;
+            }
+
+            map.put(arr[i], i);
+        }
+    }
+}
