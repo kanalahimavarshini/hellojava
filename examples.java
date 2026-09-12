f255676a82450f1664054d3b4010f3e3345149d6
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-12 09:16:36 +0530

    23

--- examples.java
+++ examples.java
@@ -358,3 +358,18 @@ class Main {
         System.out.println(sum == temp);
     }
 }
+// 19. GCD of Two Numbers
+class Main {
+    public static void main(String[] args) {
+        int a = 48;
+        int b = 18;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println(a);
+    }
+}
