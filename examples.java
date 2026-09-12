ca2e175a5222502abc1e7d7c7b3c56cd13dc52b3
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-12 09:14:42 +0530

    18

--- examples.java
+++ examples.java
@@ -257,3 +257,26 @@ class Main {
             System.out.print(x + " ");
     }
 }
+// 14. Find Duplicate Number
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 2};
+
+        int slow = arr[0];
+        int fast = arr[0];
+
+        do {
+            slow = arr[slow];
+            fast = arr[arr[fast]];
+        } while (slow != fast);
+
+        slow = arr[0];
+
+        while (slow != fast) {
+            slow = arr[slow];
+            fast = arr[fast];
+        }
+
+        System.out.println(slow);
+    }
+}
