f0801e6473a6fadea33e41606b60a58daa5133b3
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-13 14:37:02 +0530

    26

--- examples.java
+++ examples.java
@@ -410,3 +410,18 @@ marks.put("DBMS", 85);
 
 System.out.println(marks.get("Java")); // 90
 System.out.println(marks.containsKey("DBMS")); // true
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 2; i++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
