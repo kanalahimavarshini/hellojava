2ecfea4e603809a85a6b6d8e7977d275732f6683
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-13 14:40:48 +0530

    Sort Array Without Arrays.sort()

--- examples.java
+++ examples.java
@@ -509,3 +509,22 @@ public class Main {
         System.out.println("Missing = " + (expected - actual));
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 3};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+    }
+}
