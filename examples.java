710a14a337d490f0959a34d24ce7e90cf5de9fb4
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-13 14:42:30 +0530

    Remove Duplicate Elements

--- examples.java
+++ examples.java
@@ -528,3 +528,18 @@ public class Main {
         }
     }
 }
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        Set<Integer> set = new LinkedHashSet<>();
+
+        for (int n : arr) {
+            set.add(n);
+        }
+
+        System.out.println(set);
+    }
+}
