0f449049e2985979b9364c39da5bf2b5dda7d5d8
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-13 14:37:21 +0530

    27

--- examples.java
+++ examples.java
@@ -425,3 +425,23 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[][] a = {
+            {1, 2},
+            {3, 4}
+        };
+
+        int[][] b = {
+            {5, 6},
+            {7, 8}
+        };
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print((a[i][j] + b[i][j]) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
