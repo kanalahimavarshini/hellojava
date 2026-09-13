fdf41e0bc26c70a264f1491db079ab023c446140
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-13 14:37:50 +0530

    28

--- examples.java
+++ examples.java
@@ -445,3 +445,23 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 18;
+
+        int x = a;
+        int y = b;
+
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+        int lcm = (a * b) / gcd;
+
+        System.out.println("LCM = " + lcm);
+    }
+}
