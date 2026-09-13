2bef2e2c1504a362e6a5b834bd852d2687a12400
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-13 14:40:12 +0530

    Find Missing Number

--- examples.java
+++ examples.java
@@ -493,3 +493,19 @@ public class Main {
         System.out.println("Vowels = " + count);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 6};
+
+        int n = 6;
+        int expected = n * (n + 1) / 2;
+
+        int actual = 0;
+
+        for (int x : arr) {
+            actual += x;
+        }
+
+        System.out.println("Missing = " + (expected - actual));
+    }
+}
