8a9426bb7629afa87cba548f60f89fd398638e62
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-13 14:39:56 +0530

    Count Vowels

--- examples.java
+++ examples.java
@@ -479,3 +479,17 @@ public class Main {
         System.out.println("GCD = " + a);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int count = 0;
+
+        for (char c : str.toLowerCase().toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("Vowels = " + count);
+    }
+}
