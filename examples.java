e698c284316965b7657aa2e2ce453a6892cc8ec1
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-14 15:07:08 +0530

    Reverse an Array

--- examples.java
+++ examples.java
@@ -654,3 +654,24 @@ public class Main {
         System.out.println("Smallest = " + min);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+    }
+}
