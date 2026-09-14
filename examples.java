0d160c5b00a61b1b5fdf98b98bd1eb90a8d2fcea
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-14 15:09:35 +0530

    Frequency of Array Elements

--- examples.java
+++ examples.java
@@ -715,3 +715,20 @@ public class Main {
         System.out.println(binary);
     }
 }
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 1, 2, 4};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int n : arr) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            System.out.println(key + " = " + map.get(key));
+        }
+    }
+}
