c09458f557dee3638d0f9505067b6f21b7349ccf
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-14 15:08:12 +0530

    Find Duplicate Element

--- examples.java
+++ examples.java
@@ -675,3 +675,17 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 3};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate = " + arr[i]);
+                    return;
+                }
+            }
+        }
+    }
+}
