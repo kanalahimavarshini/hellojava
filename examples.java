860884c1a464674dac23f063e994e7be8a8f731f
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-14 15:08:50 +0530

    Check Perfect Number

--- examples.java
+++ examples.java
@@ -689,3 +689,16 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int n = 28;
+        int sum = 0;
+
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0)
+                sum += i;
+        }
+
+        System.out.println(sum == n ? "Perfect Number" : "Not Perfect Number");
+    }
+}
