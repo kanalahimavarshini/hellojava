ccb2eb48611179b0464298b59e625ca97017c4cd
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-15 18:29:45 +0530

    Check If Array Is Sorted

--- examples.java
+++ examples.java
@@ -900,3 +900,19 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        boolean sorted = true;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                sorted = false;
+                break;
+            }
+        }
+
+        System.out.println(sorted ? "Sorted" : "Not Sorted");
+    }
+}
