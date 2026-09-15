3c79caed58eba459f1b0a347e89124e9c6136646
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-15 18:29:02 +0530

    Find Common Elements in Two Arrays

--- examples.java
+++ examples.java
@@ -868,3 +868,18 @@ public class Main {
         System.out.println(original == sum ? "Strong Number" : "Not Strong Number");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {3, 4, 5, 6, 7};
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    System.out.print(a[i] + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
