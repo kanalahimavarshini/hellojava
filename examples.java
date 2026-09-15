588bf91b839f82d3dc9ff9a21f6b3a313f2adfd0
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-15 18:30:06 +0530

    Maximum Subarray Sum (Kadane's Algorithm)

--- examples.java
+++ examples.java
@@ -916,3 +916,18 @@ public class Main {
         System.out.println(sorted ? "Sorted" : "Not Sorted");
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int current = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            current = Math.max(arr[i], current + arr[i]);
+            max = Math.max(max, current);
+        }
+
+        System.out.println("Maximum subarray sum = " + max);
+    }
+}
