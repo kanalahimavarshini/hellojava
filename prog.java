d60689f6faa5d29167185ce1b425d07e827a35e9
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:33:41 +0530

    Linear Search in an Array

--- prog.java
+++ prog.java
@@ -1122,3 +1122,22 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 30, 45, 50};
+        int target = 45;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Found at index " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            System.out.println("Not Found");
+    }
+}
