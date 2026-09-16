4b7155b5e376aa412067d7abe2e0e6901bfeb271
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:35:39 +0530

    Find Leaders in an Array

--- basic.java
+++ basic.java
@@ -1053,3 +1053,19 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+
+        int max = arr[arr.length - 1];
+
+        System.out.print(max + " ");
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
+                System.out.print(max + " ");
+            }
+        }
+    }
+}
