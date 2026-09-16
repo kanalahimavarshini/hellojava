952f37f454ed00274027cba4c8c19ad055801de9
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:31:06 +0530

    Prime Numbers Between Two Values

--- examples.java
+++ examples.java
@@ -1061,3 +1061,26 @@ public class Main {
         System.out.println("Zero = " + zero);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int start = 10;
+        int end = 50;
+
+        for (int n = start; n <= end; n++) {
+            boolean prime = true;
+
+            if (n < 2)
+                prime = false;
+
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+
+            if (prime)
+                System.out.print(n + " ");
+        }
+    }
+}
