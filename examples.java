3878e14fa9e36fe4607cefb48f9b8b345dfe4e7c
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:29:32 +0530

    Average of Array

--- examples.java
+++ examples.java
@@ -1025,3 +1025,17 @@ public class Main {
         System.out.println("Sum = " + sum);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int n : arr) {
+            sum += n;
+        }
+
+        double average = (double) sum / arr.length;
+
+        System.out.println("Average = " + average);
+    }
+}
