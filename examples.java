baa1eb62d0a606ecf7c56cf4aa85830eca0d44ca
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:27:41 +0530

    Number of Digits That Are Even

--- examples.java
+++ examples.java
@@ -996,3 +996,20 @@ class Demo {
         g.sayHello();
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int n = 123456789;
+        int count = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit % 2 == 0)
+                count++;
+
+            n /= 10;
+        }
+
+        System.out.println("Even digits = " + count);
+    }
+}
