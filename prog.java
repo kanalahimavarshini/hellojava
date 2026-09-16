060b671d9976899879adc9c097c6371a511beb57
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:33:13 +0530

    Rotate Array Right by One Position

--- prog.java
+++ prog.java
@@ -1105,3 +1105,20 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+    }
+}
