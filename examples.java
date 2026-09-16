a34e3154f35bc10db108a6c2311ff0c36b81a313
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:29:04 +0530

    Sum of Array

--- examples.java
+++ examples.java
@@ -1013,3 +1013,15 @@ public class Main {
         System.out.println("Even digits = " + count);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int n : arr) {
+            sum += n;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
