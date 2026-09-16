8b0e3ce4b4a60bdbf6c87ec02a7cf6c57c3ed758
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:39:01 +0530

    Check Armstrong Number for Any Number of Digits

--- basic.java
+++ basic.java
@@ -1088,3 +1088,37 @@ public class Main {
         System.out.println("Smallest = " + min);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int n = 1634;
+        int original = n;
+        int digits = 0;
+        int sum = 0;
+
+        int temp = n;
+
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = n;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            int power = 1;
+
+            for (int i = 1; i <= digits; i++) {
+                power *= digit;
+            }
+
+            sum += power;
+            temp /= 10;
+        }
+
+        if (sum == original)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
