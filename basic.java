b034c891e15e26dfa89ae8543cbfcaf2106e7ebe
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-16 21:38:38 +0530

    Find Largest and Smallest in One Loop

--- basic.java
+++ basic.java
@@ -1069,3 +1069,22 @@ public class Main {
         }
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 7, 89, 23};
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        System.out.println("Largest = " + max);
+        System.out.println("Smallest = " + min);
+    }
+}
