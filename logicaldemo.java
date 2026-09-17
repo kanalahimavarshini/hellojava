ac9b2f7ceced40754814b20fc9a6c83880c7f7c0
Himavarshini <kanalahimavarshini@gmail.com>
2026-09-17 21:54:22 +0530

    code

--- logicaldemo.java
+++ logicaldemo.java
@@ -1038,3 +1038,22 @@ public class Main {
             System.out.println("No pair found");
     }
 }
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, h * (right - left));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
